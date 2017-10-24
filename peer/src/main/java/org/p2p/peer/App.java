383b9c0d81f70353953c4acfb1f1b7198bc7bb8d
atit-shetty <atit.shetty@hcl.com>
2017-10-24 12:17:30 -0400

    created maven projects

---
+++ peer/src/main/java/org/p2p/peer/App.java
@@ -0,0 +1,13 @@
+package org.p2p.peer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
