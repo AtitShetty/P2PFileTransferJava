534575971e2bba42fa940634bfa4fbdafde5407d
akshetty <akshetty@ncsu.edu>
2017-10-24 14:10:32 -0400

    socket programming init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # P2PFileTransferJava
 A P2P file transfer system based on Java
+
+
+## How to run?
+
+This sproject has two maven projects.
+
+The central project has the code for centralized server.
+
+The peer project has the code for peer servers.
+
+First build both the projects using "mvn package"
+
+Then go to central/target and run 
+
+''' java -jar central-0.0.1-jar-with-dependencies.jar '''
+
+The go to peer/targer and run
+
+''' java -jar peer-0.0.1-jar-with-dependencies.jar '''
+
+
+Run both of them in different consoles.
+
+You should see an acknowledgement in peer console.
--- central/src/main/java/org/p2p/central/App.java
+++ central/src/main/java/org/p2p/central/App.java
@@ -1,13 +1,70 @@
 package org.p2p.central;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+
+	private static ServerSocket serverSocket;
+
+	public static void main(String[] args) throws IOException
     {
         System.out.println( "Hello World!" );
+
+		start(65423);
     }
+
+	public static void start(int port) throws IOException {
+
+		serverSocket = new ServerSocket(port);
+
+		while (true) {
+			new RequestHandler(serverSocket.accept()).run();
+		}
+	}
+
+	public void stop() throws IOException {
+		serverSocket.close();
+	}
+
+	private static class RequestHandler extends Thread {
+
+		private Socket clientSocket;
+
+		public RequestHandler(Socket socket) {
+			this.clientSocket = socket;
+		}
+
+		public void run() {
+
+			try {
+				ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+
+				ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
+
+				String input = (String) is.readObject();
+
+				System.out.println("Input from peer" + input);
+
+				os.writeObject("Received input:" + input);
+
+			} catch (Exception e) {
+				System.out.println("cannot fulfil request" + e.getMessage());
+			} finally {
+				try {
+					this.clientSocket.close();
+				} catch (IOException e) {
+					System.out.println("Cannot close socket");
+				}
+			}
+		}
+	}
 }
